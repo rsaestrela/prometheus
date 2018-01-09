2048f444e9cafcc283ee9bbd551fe2edccafed8c
Raul Estrela <restrela@criticalsoftware.com>
2018-01-09 18:03:27 +0000

    fixing json serialization

--- src/main/kotlin/com/github/rsaestrela/prometheus/PrometheusInPractice.kt
+++ src/main/kotlin/com/github/rsaestrela/prometheus/PrometheusInPractice.kt
@@ -1,8 +1,20 @@
 package com.github.rsaestrela.prometheus
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import io.prometheus.client.CollectorRegistry
+import io.prometheus.client.Counter
+import io.prometheus.client.SimpleCollector
+import io.prometheus.client.exporter.MetricsServlet
+import io.prometheus.client.hotspot.DefaultExports
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.web.servlet.ServletRegistrationBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
 import org.springframework.stereotype.Service
@@ -20,76 +32,76 @@ fun main(args: Array<String>) {
 }
 
 data class QuestionnaireRequest(var useSocialNetworkSites: Boolean,
-                                var socialSitesMemberOf: ArrayList<SocialSites>,
+                                var socialSitesMemberOf: ArrayList<String>,
                                 var manyHoursSpendSocialSitesAWeek: Int,
                                 var informationIncludedSocialSites: ArrayList<String>)
-
-
-enum class SocialSites {
-    FACEBOOK,
-    INSTAGRAM,
-    HI5,
-    LINKEDIN,
-    PINTEREST,
-}
-
-enum class InformationIncludedSocialSites {
-    EMAIL_ADDRESS,
-    HOME_TOWN_CITY,
-    IM_ADDRESS,
-    PHONE_NUMBER,
-    PHOTOS_OTHERS,
-    PHOTOS_YOURSELF,
-    POLITICAL_VIEW,
-    REAL_NAME,
-    RELATIONSHIP_STATUS,
-    SEXUAL_ORIENTATION
-}
-
-enum class ResponseCode(private val response: String) {
-
-    SUCCESS("success"),
-    BAD_REQUEST("invalid request");
-
-    override fun toString(): String {
-        return response
-    }
-
-}
 
 @Controller
 @RequestMapping("/")
 class SocialQuestionnaireController(@Autowired val service: SocialQuestionnaireService) {
 
     @RequestMapping(method = [(RequestMethod.POST)])
-    fun indexPost(@RequestBody request: QuestionnaireRequest): ResponseEntity<ResponseCode> {
-
-        val validationResult = service.validRequest(request)
-        validationResult.first.not().apply {
-            return ResponseEntity.badRequest().body(validationResult.second)
-        }
-
-        return ResponseEntity.ok(validationResult.second)
+    fun indexPost(@RequestBody request: QuestionnaireRequest): ResponseEntity<String> {
+        return ResponseEntity.ok("Success")
     }
 }
 
 @Service
-class SocialQuestionnaireService {
+class SocialQuestionnaireService
 
-    fun validRequest(request: QuestionnaireRequest): Pair<Boolean, ResponseCode> {
-        return when (request.useSocialNetworkSites &&
-                (request.socialSitesMemberOf.isEmpty() ||
-                        request.manyHoursSpendSocialSitesAWeek == 0 ||
-                        request.informationIncludedSocialSites.isEmpty())) {
-            true -> Pair(false, ResponseCode.BAD_REQUEST)
-            false -> Pair(true, ResponseCode.SUCCESS)
+@Service
+class PrometheusMetrics {}
+
+@Configuration
+class ObjectMapperConfiguration {
+    @Bean
+    @Primary
+    fun objectMapper() = ObjectMapper().apply {
+        registerModule(KotlinModule())
+    }
+}
+
+@Configuration
+class PrometheusConfiguration {
+
+    @Value("\${prometheus.metrics.endpoint}")
+    val metricsEndpoint: String? = null
+
+    @Bean
+    fun initCollectorRegistry(): CollectorRegistry {
+        val collectorRegistry = CollectorRegistry.defaultRegistry
+        collectorRegistry.clear()
+        DefaultExports.initialize()
+        MetricInstrument.allCollectors().forEach { c ->
+            run {
+                c.register<SimpleCollector<*>>()
+            }
+        }
+        return collectorRegistry
+    }
+
+    @Bean
+    fun registerPrometheusExporterServlet(metricRegistry: CollectorRegistry): ServletRegistrationBean {
+        return ServletRegistrationBean(MetricsServlet(metricRegistry), metricsEndpoint)
+    }
+
+}
+
+enum class MetricInstrument constructor(val collector: SimpleCollector<*>) {
+
+    USE_SOCIAL_SITES(Counter.build().name("use_social_sites_counter")
+            .labelNames(MetricInstrumentLabel.USE_SOCIAL_SITES.desc)
+            .help("Use social sites counter")
+            .create());
+
+    companion object {
+        fun allCollectors(): List<SimpleCollector<*>> {
+            return MetricInstrument.values().map { c -> c.collector }.toList()
         }
     }
 
 }
 
-@Service
-class PrometheusMetrics {
-
-
+enum class MetricInstrumentLabel constructor(val desc: String) {
+    USE_SOCIAL_SITES("use_social_sites")
 }
